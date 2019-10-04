07856ea48327536c8828c61d585a2fafd830d896
Patty Rosendaal <sudo_patty@packer-Ubuntu-18.04.1-Server>
2019-10-04 13:34:58 +0200

    df

--- R/hello.R
+++ R/hello.R
@@ -10,7 +10,7 @@
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
+#   Check Package:     sdfsdfsd        'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
